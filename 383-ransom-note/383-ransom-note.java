0cc7a9dd93137e9f766c7e223ad35839beed13bc
Cedric <83405394+Cedric2107@users.noreply.github.com>
2022-08-16 15:20:23 +0200

    Time: 5 ms (74.47%), Space: 46.7 MB (49.08%) - LeetHub

---
+++ 383-ransom-note/383-ransom-note.java
@@ -0,0 +1,21 @@
+import java.util.HashMap;
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        char[] rN = ransomNote.toCharArray();
+        char[] mag = magazine.toCharArray();
+
+        for (int i = 0; i< rN.length;i++){
+            boolean hit = false;
+            for (int j = 0; j <mag.length && hit==false;j++){
+                if(rN[i]==mag[j]){
+                    mag[j]='\u0000';
+                    hit=true;
+                }            
+            }
+            if(hit==false)
+                return false;
+            
+    }
+        return true;
+    }
+}
